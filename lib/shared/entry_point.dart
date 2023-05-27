4c736f67bbcc06385ef85ae095508b7f2752dbf8
benjamin batundi <benbatundi7@gmail.com>
2023-05-27 21:35:30 +0200

    done with all ui

--- lib/shared/entry_point.dart
+++ lib/shared/entry_point.dart
@@ -1,28 +1,32 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:ui_admin_responsive/app/ui/home/home.dart';
-import 'package:ui_admin_responsive/app/ui/shared/style.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:ui_admin_responsive/constants.dart';
+import 'package:ui_admin_responsive/controllers/MenuAppController.dart';
+import 'package:ui_admin_responsive/screens/main/main_screen.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.white,
-          systemNavigationBarIconBrightness: Brightness.dark,
-          statusBarColor: Colors.white,
-          statusBarIconBrightness: Brightness.dark,
-        ),
-      );
-    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: Style.themeData(false, context),
-      home: const Home(),
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
     );
   }
 }
